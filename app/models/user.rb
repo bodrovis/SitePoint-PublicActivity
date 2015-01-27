d666117033140e72fa4ad4322a343677aecd66e8
bodrovis <ibodrov@campaigner.ru>
2015-01-27 18:10:40 +0300

    clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
     def from_omniauth(auth)
       user = User.find_or_initialize_by(uid: auth['uid'])
       user.name = auth['info']['name']
+      user.avatar_url = auth['info']['image']
       user.save!
       user
     end
